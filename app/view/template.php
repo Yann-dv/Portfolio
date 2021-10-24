6dcdc70065e1c05184320c7f5099b698a5601a40
Yann Dev <h.yann_pro@yahoo.fr>
2021-10-25 00:36:55 +0200

    Add desc

--- app/view/template.php
+++ app/view/template.php
@@ -1,20 +1,24 @@
 <?php
 
-$title = 'Website title';
-$content = '<h1>Main title</h1>';
+$title = 'Développement Web - Yann Hagege ';
+$page_title = 'Développement Web';
+$description = 'Bonjour, je m\'appelle Yann ;)'
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr-FR">
     <head>
         <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
         <link href="./public/css/style.css" rel="stylesheet" /> 
     </head>
         
     <body>
-        <?= $content ?>
+        <?php echo '<h1 class="main_title">' . $page_title. '</h1>' ?>
+        <?php echo '<div class="description"><p>' . $description . '</p></div>'?>
     </body>
 </html>
 
