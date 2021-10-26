7648bab82c54674ddf08c1dc4cbf56244873998c
Yann Dev <h.yann_pro@yahoo.fr>
2021-10-26 11:29:58 +0200

    Skills layout and animations

--- app/view/skills.php
+++ app/view/skills.php
@@ -22,23 +22,23 @@
 					<li class="item-skills">
 						<strong>Applications spécifiques et Interface d'administration</strong>
 						<br>
-						<span class="skillsDesc"><abbr title="Hypertext Preprocessor">PHP</abbr> - <abbr title="My Structured Query Language">MySQL</abbr> - <abbr title="Asynchronous JavaScript and XML">AJAX</abbr> / 
-						<abbr title="Active Server Pages">ASP</abbr>.NET - <abbr title="C Sharp">C#</abbr> - <abbr title="Structured Query Language">SQL</abbr></span>										
+						<span class="skillsDesc"><abbr title="Hypertext Preprocessor">PHP</abbr> - <abbr title="My Structured Query Language">MySQL</abbr> - <abbr title="Structured Query Language from POSTGRE">PostgreSQL</abbr>
+                        / <abbr title="Structured Query Language">SQL</abbr></span>										
 					</li>
 					<li class="item-skills">
 						<strong>Mise en place / Modification / Gestion de <abbr title="Content Management System">CMS</abbr></strong> 
 						<br>
-						<em class="skillsDesc">Wordpress, Joomla, Prestashop, phpBB, IPBoard</em>											
+						<em class="skillsDesc">Wordpress, Wix</em>											
 					</li>
 					<li class="item-skills">
-						<strong> Intégration <abbr title="(eXtensible) HyperText Markup Language">(X)HTML</abbr> / <abbr title="Cascading Style Sheets">CSS</abbr></strong>
+						<strong> Intégration <abbr title="HyperText Markup Language">HTML</abbr> / <abbr title="Cascading Style Sheets">CSS</abbr></strong>
 						<br>
-						<em class="skillsDesc">Codage à la main respectueux des standards du Web</em>
+						<em class="skillsDesc">Codage à partir de 0 en respectant les standards du développement Web</em>
 					</li>
 					<li class="item-skills">
-						<strong>Dynamise des pages par <abbr title="Langage de développement web orienté client">JavaScript</abbr> / <abbr title="Asynchronous JavaScript and XML">AJAX</abbr></strong> 
+						<strong>Dynamise des pages par <abbr title="Langage de développement web orienté client">JavaScript</abbr></strong> 
 						<br>
-						<em class="skillsDesc">jQuery, Prototype, Mootools, Scriptaculous</em>
+						<em class="skillsDesc">jQuery, Express, Node.Js, VueJS</em>
 					</li>
 					<li class="item-skills">
 						<strong>Validation <abbr title="World Wide Web Consortium">W3C</abbr>, <abbr title="Web Accessibility Initiative">WAI</abbr> &amp; référencement naturel <abbr title="Search Engine Optimisation">SEO</abbr></strong>
@@ -57,11 +57,11 @@
         <div class="rightSkillsList">
             <div class="skillsLevel">
                 <ul class="skillsList_dev">
-                <h4><?php echo strtoupper('competences en developpement'); ?></h4>
+                <h4><?php echo strtoupper('Languages de programmation'); ?></h4>
                     <li class="item-skills">
                         <div class="progress-bar">
-                            <span data-value="75">HTML5/CSS3</span>
-                            <progress value="75" max="100"></progress>
+                            <span data-value="85">HTML5/CSS3</span>
+                            <progress value="85" max="100"></progress>
                         </div>
 					</li>
                     <li class="item-skills">
@@ -83,8 +83,35 @@
                         </div>
 					</li>
                 </ul>
+                <ul class="skillsList_tools">
+                <h4><?php echo strtoupper('Outils de developpement : frameworks, CMS'); ?></h4>
+                <li class="item-skills">
+                        <div class="progress-bar">
+                            <span data-value="75">Bootstrap, Symfony</span>
+                            <progress value="75" max="100"></progress>
+                        </div>
+					</li>
+                    <li class="item-skills">
+                        <div class="progress-bar">
+                            <span data-value="70">VueJS, ExpressJS, Node.JS</span>
+                            <progress value="70" max="100"></progress>
+                        </div>
+					</li>
+                    <li class="item-skills">
+                        <div class="progress-bar">
+                            <span data-value="70">Api REST</span>
+                            <progress value="70" max="100"></progress>
+                        </div>
+					</li>
+                    <li class="item-skills">
+                        <div class="progress-bar">
+                            <span data-value="80">Wordpress, Wix</span>
+                            <progress value="80" max="100"></progress>
+                        </div>
+					</li>
+                </ul>
                 <ul class="skillsList_design">
-                <h4><?php echo strtoupper('competences en conception graphique'); ?></h4>
+                <h4><?php echo strtoupper('Outils de conception graphique'); ?></h4>
                 <li class="item-skills">
                         <div class="progress-bar">
                             <span data-value="75">PHOTOSHOP</span>
@@ -93,19 +120,7 @@
 					</li>
                     <li class="item-skills">
                         <div class="progress-bar">
-                            <span data-value="70">ILLUSTRATOR</span>
-                            <progress value="70" max="100"></progress>
-                        </div>
-					</li>
-					<li class="item-skills">
-                        <div class="progress-bar">
-                            <span data-value="60">PHP</span>
-                            <progress value="60" max="100"></progress>
-                        </div>
-					</li>
-					<li class="item-skills">
-                        <div class="progress-bar">
-                            <span data-value="50">MySQL, PostgreSQL</span>
+                            <span data-value="50">ILLUSTRATOR</span>
                             <progress value="50" max="100"></progress>
                         </div>
 					</li>
