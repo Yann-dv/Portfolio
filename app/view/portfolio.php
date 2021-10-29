f44b12c9602bd729921996dcae22f1e3367aec8a
Yann Dev <h.yann_pro@yahoo.fr>
2021-10-29 20:53:51 +0200

    links title

--- app/view/portfolio.php
+++ app/view/portfolio.php
@@ -19,7 +19,7 @@
           //don't show it
         }
         else {
-          echo '<a href="https://yann-dv.github.io/' . $project['name'] . '" target="_blank">'
+          echo '<a href="https://yann-dv.github.io/' . $project['name'] . '" target="_blank" title="Cliquer pour visualiser">'
           . '<div class="project" id="project_'. array_search($project, $projects_list) . '">' 
           . '<h3 class="project_title">' . $project['name'] . '</h3>'
           . '<image src="./public/image/min_' . $project['name'] . '.jpg"' . '>'
