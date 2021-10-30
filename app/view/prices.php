501c8e5a7f532e64a5209024646e158e323e7fef
Yann Dev <h.yann_pro@yahoo.fr>
2021-10-30 11:22:06 +0200

    prices layout

--- app/view/prices.php
+++ app/view/prices.php
@@ -6,43 +6,43 @@
 </div>
 <div class="pricesContainer">
     <div class="prices classic_website">
-      <?php echo '<p class="priceHeader">' . strtoupper('Site vitrine personnalise') . '' . '<hr class="under_border"/>'?>
-		<p>À partir de 1000€ *</p>
+      <?php echo '<span class="priceHeader">' . strtoupper('Site vitrine personnalise') . '</span>' . '<hr class="under_border"/>'?>
+		<span class="first-priceList-color">À partir de 1000€ *</span>
 			<span class="second-priceList-color">
 				Développé sous Wordpress (Outil de gestion de contenu)
 			</span>
-			<span>
+			<span class="first-priceList-color">
 				
 				Charte graphique à partir d'une maquette pré-définie
 			</span>
 				<span class="second-priceList-color">
 					Sélection d'une palette de couleur
 				</span>
-				<span>
+				<span class="first-priceList-color">
 					Domaine et hébergement + e-mail pendant 12 mois
 				</span>						
 					<span class="second-priceList-color">
 						Interface administrateur standard
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Site responsive (adapté tous supports)
 					</span>
 					<span class="second-priceList-color">
 						Mise en place du contenu (textes &amp; images)
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Formulaire de contact
 					</span>
 					<span class="second-priceList-color">
 						Liens de partage sur les réseaux sociaux
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Optimisation du référencement naturel
 					</span>
 					<span class="second-priceList-color">
 						Statistiques de visite
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Mise en ligne du site
 					</span>
 					<span class="second-priceList-color">
@@ -51,19 +51,19 @@
 	    <a href="#" >DEMANDER UN DEVIS</a>
 	</div>
     <div class="prices custom_website">
-        <?php echo '<p class="priceHeader">' . strtoupper('Site vitrine personnalise') . ''  . '<hr class="under_border"/>'?>
-		<p>À partir de 1600€ *</p>
+        <?php echo '<span class="priceHeader">' . strtoupper('Site vitrine personnalise') . '<br/>' . '</span> <hr class="under_border"/>'?>
+		<span class="first-priceList-color">À partir de 1600€ *</span>
 			<span class="second-priceList-color">
 				 Développé sous Wordpress (Outil de gestion de contenu)
 			</span>
-			<span>
+			<span class="first-priceList-color">
 				
 				Charte graphique tirée d'une maquette pré-définie
 			</span>
 				<span class="second-priceList-color">
 					Sélection d'une palette de couleur
 				</span>
-				<span>
+				<span class="first-priceList-color">
 					
 					Domaine et hébergement + e-mail pendant 12 mois
 				</span>						
@@ -71,7 +71,7 @@
 					    
 						Interface administrateur standard
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Site responsive (adapté tous supports)
 					</span>
@@ -79,7 +79,7 @@
 						
 						Mise en place du contenu (textes &amp; images)
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Formulaire de contact
 					</span>
@@ -87,14 +87,14 @@
 						
 						Liens de partage sur les réseaux sociaux
 					</span>
-					<span>
+					<span class="first-priceList-color">
 
 						Optimisation du référencement
 					</span>
 					<span class="second-priceList-color">
 						Statistiques de visite
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Mise en ligne du site
 					</span>
@@ -105,31 +105,31 @@
 	    <a href="#" >DEMANDER UN DEVIS</a>
 	</div>
     <div class="prices ecommerce">
-        <?php echo '<p class="priceHeader">' . strtoupper('Site') . '<br/>' . strtoupper('e-commerce') . '' . '<hr class="under_border"/>' ?>
-		<p>À partir de 3000 € *</p>
+        <?php echo '<span class="priceHeader">' . strtoupper('Site') . '<br/>' . strtoupper('e-commerce') . '<br/>' . '</span> <hr class="under_border"/>' ?>
+		<span class="first-priceList-color">À partir de 3000 € *</span>
             <span class="second-priceList-color">
 				Développé sous WORDPRESS (Outil de gestion de contenu)
 			</span>
-			<span>
+			<span class="first-priceList-color">
 				Charte graphique à partir d'une maquette pré-définie
 			</span>
 			<span class="second-priceList-color">
 				Sélection d'une palette de couleur
 			</span>
-			<span>
+			<span class="first-priceList-color">
 				Domaine + hébergement + e-mail pendant 12 mois
 			</span>						
 		    <span class="second-priceList-color">
 				Interface administrateur - STANDARD
 			</span>
-			<span>		
+			<span class="first-priceList-color">		
 			    Site responsive (adapté tous supports)			
 			</span>
 			<span class="second-priceList-color">		
 						
 						Mise en place du contenu (textes &amp; images)
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Formulaire de contact
 					</span>
@@ -137,13 +137,13 @@
 						
 						Liens de partage sur les réseaux sociaux
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Optimisation du référencement
 					</span>
 					<span class="second-priceList-color">
 						Statistiques de visite
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Mise en ligne du site
 					</span>
@@ -154,24 +154,24 @@
 	    <a href="#" >DEMANDER UN DEVIS</a>
 	</div>
     <div class="prices maintenance">
-        <?php echo '<p class="priceHeader">' . strtoupper('Maintenance') . '<br/>' . strtoupper('de site') . '' . '<hr class="under_border"/>' ?>
-		<p>Forfait - 400€ *</p>
+        <?php echo '<span class="priceHeader">' . strtoupper('Maintenance') . '<br/>' . strtoupper('de site') . '<br/>' . '</span> <hr class="under_border"/>' ?>
+		<span class="first-priceList-color">Forfait - 400€ *</span>
 			<span class="second-priceList-color">
 			    Développé sous Wordpress (Outil de gestion de contenu)
 			</span>
-			<span>
+			<span class="first-priceList-color">
 				Charte graphique à partir d'une maquette pré-définie
 			</span>
             <span class="second-priceList-color">
 				Sélection d'une palette de couleur
             </span>
-            <span>
+            <span class="first-priceList-color">
 				Domaine + hébergement + e-mail pendant 12 mois
 			</span>						
 			<span class="second-priceList-color"> 
 			Interface administrateur - STANDARD
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Site responsive (adapté tous supports)
 					</span>
@@ -179,7 +179,7 @@
 						
 						Mise en place du contenu (textes &amp; images)
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						
 						Formulaire de contact
 					</span>
@@ -187,13 +187,13 @@
 						
 						Liens de partage sur les réseaux sociaux
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Optimisation du référencement
 					</span>
 					<span class="second-priceList-color">
 						Statistiques de visite
 					</span>
-					<span>
+					<span class="first-priceList-color">
 						Mise en ligne du site
 					</span>
 					<span class="second-priceList-color">
