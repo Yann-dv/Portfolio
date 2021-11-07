4dd1194c0c819aa69a06257cd4ffd327496e2a92
Yann Dev <h.yann_pro@yahoo.fr>
2021-11-07 13:47:06 +0100

    About added

--- app/view/about.php
+++ app/view/about.php
@@ -1,8 +1,27 @@
 <?php ob_start(); ?>
 <div class="sectionHeader">
         <h3 class="sectionTitle"><?php echo strtoupper('A propos') ?></h3>
-        N'hésitez pas à demander un devis gratuit pour votre projet !
+        <p>Un développeur web passionné !</p>
         <hr class="under_border">
-        <p>This is my about content</p>
+</div>
+<div class="aboutContainer">
+        <img src="./public/image/avatar.jpg" alt="Image portrait de moi-même" title="Moi-même">
+        <p>
+        Ma passion pour l'informatique et la programmation commence dès l'adolescence, dans les années 2000. 
+        Gameur, passionné de technologies et codeur à mes heures perdues (je ne remercierais jamais assez l'éditeur
+        de modules de NeverWinter Nights !), cette passion n'est à l'époque qu'un 
+        loisir, que je mettrais d'ailleurs au second plan (sans jamais arrêter cependant) durant ma carrière dans 
+        les médecines alternatives.
+        En effet, après une période de travail de 10 ans dans le domaine de la santé, je suis revenu à mes 
+        premières passions : coder et développer ! C'est au début de l'année 2021 que j'entreprend de me 
+        reconvertir pour le métier de Développeur Web. Je suis alors une formation de 6 mois auprès d'
+        <a class="external_link" href="https://openclassrooms.com/fr/paths/185-developpeur-web" target="_blank">
+        Openclassrooms.
+        </a>
+        </p>
+       <p>Cette formation s'avèrera extremement riche en enseignements et en projets professionnalisants, et c'est fort des connaissances acquises durant cette formation, 
+        ainsi que celles que j'ai développé en me formant en parallèle en autodidacte que je démarre ma carrière de Développeur Web !
+        Depuis ma reconversion, j'ai pu participer à différentes projets de développement, notamment des créations de sites vitrines pour des professionnels du sport.
+        </p>
 </div>
 <?php $about= ob_get_clean(); ?>
