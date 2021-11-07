2cd620f9c37e8cbd6d9bfb7e6dabb290f4224254
Yann Dev <h.yann_pro@yahoo.fr>
2021-11-07 15:11:18 +0100

    working on form

--- app/view/contactModalForm.php
+++ app/view/contactModalForm.php
@@ -1,44 +1,43 @@
 
 <?php ob_start(); ?>
 <div class="modal_form">
+<a href="javascript:history.go(-1)" class="modal_close">&times;</a>
 	<form id="contactForm" action="" class="contact-form">
 	    <fieldset>
-			<div class="col-xs-12 col-md-6 col-lg-6">
-				<h5>Vos coordonnées</h5>
+			<div class="">
+				<h2>Vos coordonnées</h2>
 				<hr>
-	            <div class="form-group input-group">
+	            <div class="">
 	            	<span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
 	                <input type="text" name="nomDevis" class="form-control" placeholder="Nom / Prénom" autocomplete="off"> 
-	                <span class="input-group-addon">*</span>
 	            </div>
-	            <div class="form-group input-group col-xs-12 col-sm-12 col-md-12 col-lg-12 noPM">
-					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 noPM">
+	            <div class="form-group input-group">
+					<div class="inputCheck">
 						<div class="i-checks">
-    						<div class="iradio_minimal-grey checked" style="position: relative;"><input type="radio" id="idRadioP" name="typeUserDevis" value="1" checked="" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255) none repeat scroll 0% 0%; border: 0px none; opacity: 0;"></ins></div> <i></i> 
+    						<div class="iradio_minimal-grey checked"><input type="radio" id="idRadioP" name="typeUserDevis" value="1" checked="">
 							<label id="idRadioP_label" for="idRadioP" class="bold">Particulier</label>
 						</div>
 					</div>
-					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 noPM">
+					<div class="inputCheck">
 						<div class="i-checks">
-							<div class="iradio_minimal-grey" style="position: relative;"><input type="radio" id="idRadioA" name="typeUserDevis" value="2" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255) none repeat scroll 0% 0%; border: 0px none; opacity: 0;"></ins></div> <i></i> 
+							<div class="iradio_minimal-grey"><input type="radio" id="idRadioA" name="typeUserDevis" value="2">
 							<label id="idRadioA_label" for="idRadioA">Association</label>
 						</div>
 					</div>								
-					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 noPM">
+					<div class="inputCheck">
 						<div class="i-checks">
-							<div class="iradio_minimal-grey" style="position: relative;"><input type="radio" id="idRadioS" name="typeUserDevis" value="3" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255) none repeat scroll 0% 0%; border: 0px none; opacity: 0;"></ins></div> <i></i> 
+							<div class="iradio_minimal-grey"><input type="radio" id="idRadioS" name="typeUserDevis" value="3">
 							<label id="idRadioS_label" for="idRadioS">Société</label>
 						</div>
 					</div>
 	            </div>
-	            <div id="blocEnseigne" class="form-group input-group" style="display: none;" wfd-invisible="true">
-	                <input type="text" name="enseigneDevis" class="form-control" autocomplete="off"> 
-	                <span class="input-group-addon">*</span>
+	            <div id="blocCompagny" class="form-group input-group">
+                    <input type="text" name="compagnyName" class="form-control" placeholder="Société / Association :" autocomplete="off"> 
 	            </div>
 	            <div class="form-group input-group">
 	            	<span class="input-group-addon"><i class="fa fa-at" aria-hidden="true"></i></span>
 	                <input type="email" name="emailDevis" class="form-control" placeholder="E-mail" autocomplete="off"> 
-	                <span class="input-group-addon">*</span>
+	                
 	            </div>
 	            <div class="form-group input-group">
 	            	<span class="input-group-addon"><i class="fa fa-phone" aria-hidden="true"></i></span>
@@ -49,17 +48,16 @@
 	                <input type="text" name="urlDevis" class="form-control" placeholder="Adresse site internet (si existant)" autocomplete="off"> 
 	            </div>
 	    		<div class="i-checks">
-				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 noPM">
-						<div class="icheckbox_minimal-grey" style="position: relative;"><input type="checkbox" id="idChkRGPDDevis" name="chkRGPDDevis" value="1" style="position: absolute; opacity: 0;"><ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255) none repeat scroll 0% 0%; border: 0px none; opacity: 0;"></ins></div> <i></i> 
+				<div class="">
+						<div class="icheckbox_minimal-grey"><input type="checkbox" id="idChkRGPDDevis" name="chkRGPDDevis" value="1"> 
 					</div>
-					<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 noPM">
+					<div class="">
 						<label class="label_rgpd" for="idChkRGPDDevis">En soumettant ce formulaire, j'accepte que les informations saisies soient exploitées dans le cadre de la demande de devis et de la relation commerciale qui peut en découler.</label>
 					</div>
 				</div>
 				<div class="form-group input-group">
 	            	<span class="input-group-addon"><i class="fa fa-lock" aria-hidden="true"></i></span>
 	                <input type="text" name="captchaDevis" class="form-control" placeholder="5 + 4 = ?" autocomplete="off"> 
-	                <span class="input-group-addon">*</span>
 	            </div>							
 				<div class="msgFormDevis">
 		            <p class="alert alert-info">
@@ -69,11 +67,11 @@
 		            </p>								
 				</div>
 	        </div>
-		        <div class="col-xs-12 col-md-6 col-lg-6">
+		        <div class="">
 				<h5>Votre projet</h5>
 				<hr>				        					        	
 	            <div class="form-group input-group select-input-group">
-	                <span class="jcf-select jcf-unselectable"><select name="typeProjetDevis" style="position: absolute; height: 100%; width: 100%;" class="jcf-reset-appearance">
+	                <span class="jcf-select jcf-unselectable"><select name="typeProjetDevis" class="jcf-reset-appearance">
 	                	<option value="" selected="" disabled="">Type de projet</option>
 	                	<option value="Site vitrine standard">Site vitrine [Standard]</option>
 	                	<option value="Site vitrine personnalisé">Site vitrine [Personnalisé]</option>
@@ -87,7 +85,7 @@
 	            </div>
 	            <div class="form-group input-group">
 	            	<textarea class="form-control" cols="30" rows="10" name="descDevis" placeholder="Description de votre projet... Merci de détailler le plus possible votre besoin afin de faciliter nos futurs échanges."></textarea>
-	                <span class="input-group-addon">*</span>
+	                
 	           </div>
 	            <div class="form-group text-right">
 					<button class="btn btn-f-info" id="idBtnDevis" name="btnDevis">
@@ -97,6 +95,5 @@
 			</div>
 		</fieldset>			
 	</form>
-    <a href="#" class="modal_close">&times;</a>
 </div>
 <?php $contactModalForm= ob_get_clean(); ?>
