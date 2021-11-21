c9b6dcf9014eefadeafa0f09a5f1d1d207c36ddd
Yann Dev <h.yann_pro@yahoo.fr>
2021-11-21 19:47:12 +0100

    onScroll animation added

--- app/view/template.php
+++ app/view/template.php
@@ -19,25 +19,27 @@ require ('./app/textContent.php');
     <!--<body onselectstart="return false" oncontextmenu="return false" 
     ondragstart="return false" onMouseOver="window.status='No right click '; return true;">-->
     <body>
-        <?php require('./app/view/header.php'); ?>
-        <?php require('./app/view/navbar.php'); ?>
-        <?php require('./app/view/services.php'); ?>
-        <?php require('./app/view/skills.php'); ?>
-        <?php require('./app/view/portfolio.php'); ?>
-        <?php require('./app/view/prices.php'); ?>
-        <?php require('./app/view/about.php'); ?>
-        <?php require('./app/view/contact.php'); ?>
-        <?php require('./app/view/contactModalForm.php'); ?>
+        <div class="mainContent">
+            <?php require('./app/view/header.php'); ?>
+            <?php require('./app/view/navbar.php'); ?>
+            <?php require('./app/view/services.php'); ?>
+            <?php require('./app/view/skills.php'); ?>
+            <?php require('./app/view/portfolio.php'); ?>
+            <?php require('./app/view/prices.php'); ?>
+            <?php require('./app/view/about.php'); ?>
+            <?php require('./app/view/contact.php'); ?>
+            <?php require('./app/view/contactModalForm.php'); ?>
 
-        <?php echo '<nav class="navbar fadeLoad" id="navbar">' . $navbar . '</nav>'?>
-        <?php echo '<div class="header fadeLoad" id="header">' . $header . '</div>' ?>
-        <?php echo '<div class="contentBlock fadeLoad" id="services">' . $services . '</div>'?>
-        <?php echo '<div class="contentBlock fadeLoad" id="skills">' . $skills . '</div>'?>
-        <?php echo '<div class="contentBlock fadeLoad" id="portfolio">' . $portfolio . '</div>'?>
-        <?php echo '<div class="contentBlock fadeLoad" id="prices">' . $prices . '</div>'?>
-        <?php echo '<div class="contentBlock fadeLoad" id="about">' . $about . '</div>'?>
-        <?php echo '<div class="contentBlock fadeLoad" id="contact">' . $contact . '</div>'?>
-        <?php echo '<div class="modal" id="contactModal">' . $contactModalForm . '</div>'?>
+            <?php echo '<nav class="navbar fadeLoad" id="navbar">' . $navbar . '</nav>'?>
+            <?php echo '<div class="header fadeLoad" id="header">' . $header . '</div>' ?>
+            <?php echo '<div class="contentBlock fadeLoad" id="services">' . $services . '</div>'?>
+            <?php echo '<div class="contentBlock fadeLoad" id="skills">' . $skills . '</div>'?>
+            <?php echo '<div class="contentBlock fadeLoad" id="portfolio">' . $portfolio . '</div>'?>
+            <?php echo '<div class="contentBlock fadeLoad" id="prices">' . $prices . '</div>'?>
+            <?php echo '<div class="contentBlock fadeLoad" id="about">' . $about . '</div>'?>
+            <?php echo '<div class="contentBlock fadeLoad" id="contact">' . $contact . '</div>'?>
+            <?php echo '<div class="modal" id="contactModal">' . $contactModalForm . '</div>'?>
+        </div>
     </body>
 </html>
 
