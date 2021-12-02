f7beb8ce08ac31a310a0186ea8d5283bd12b9ffd
Yann Dev <h.yann_pro@yahoo.fr>
2021-12-02 11:20:41 +0100

    sleep timer reduced

--- back/traitement.php
+++ back/traitement.php
@@ -79,7 +79,7 @@ if(check_token($_POST['g-recaptcha-response'], $reCAPTCHA_secret_key)) {
             try {
                 $_SESSION['sendedContent'] = $mailContent;
                 $response = $sendgrid->send($email);
-                sleep(1);
+                sleep(.5);
                 header('Location: ../app/view/success.php');
                 // Display for debug //
                 /*
