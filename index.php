48ed8877df260ff2c2d07f52bfcb0e23e7752df0
Yann Dev <h.yann_pro@yahoo.fr>
2021-12-14 16:25:34 +0100

    compress imgs, minify css, try nonce for scripts

--- index.php
+++ index.php
@@ -13,15 +13,28 @@ require ('./app/textContent.php');
         <meta name="description" content="Yann H. vous aide à concevoir et produire différentes solutions digitales pour votre entreprise : création de site web vitrine, site e-commerce, chat/forum d'entreprise, et pleins d'autres solutions pour vous aider à vous développer grâce aux outils informatiques et la puissance du web.">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 
+        <!--Robots meta tags-->
+        <meta name="robots" content="index, follow">
+        <meta property="og:title" content="Yann Hagege - Création de sites et applications Web">
+        <meta property="twitter:title" content="Yann Hagege - Création de sites et applications Web">
+        <meta property="og:description" content="Yann H. vous aide à concevoir et produire différentes solutions digitales pour tous vos projets de sites/applications web.">
+        <meta property="twitter:description" content="Yann H. vous aide à concevoir et produire différentes solutions digitales pour tous vos projets de sites/applications web.">
+        <meta property="og:image" content="https://portfolio-yh-dev.herokuapp.com/public/image/socials_micro_banner.jpg">
+        <meta property="twitter:image" content="https://portfolio-yh-dev.herokuapp.com/public/image/socials_micro_banner.jpg">
+        <meta property="twitter:card" content="summary_large_image">
+        
+        <!---->
+
+
         <title><?php echo $title ?></title>
         <link rel="icon" href="././public/image/favicon.ico">
-        <link href="./public/css/style.css" rel="stylesheet"/>
-        <script src="https://kit.fontawesome.com/158c254b57.js"></script>
-        <script async src="./app.js" defer></script>
+        <link href="./public/css/style_minified.css" rel="stylesheet"/>
+        <script nonce="rAnd0m" src="https://kit.fontawesome.com/158c254b57.js"></script>
+        <script nonce="rAnd0m" async src="./app.js" defer></script>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JRQKGGFVH4"></script>
-        <script>
+        <script nonce="rAnd0m" async src="https://www.googletagmanager.com/gtag/js?id=G-JRQKGGFVH4"></script>
+        <script nonce="rAnd0m">
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
