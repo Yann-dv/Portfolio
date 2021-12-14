9a8d2126004c5506d407a3fe6874a2dc1d70cb4d
Yann Dev <h.yann_pro@yahoo.fr>
2021-12-14 11:24:30 +0100

    starting seo optimization

--- app/view/template.php
+++ app/view/template.php
@@ -1,6 +1,5 @@
 <?php
 
-require ('./app/textContent.php');
 require('./app/view/header.php'); 
 require('./app/view/navbar.php'); 
 require('./app/view/services.php');
@@ -11,21 +10,7 @@ require('./app/view/about.php');
 require('./app/view/contact.php');
 require('./app/view/contactModalForm.php');
 
-?>
-
-<!DOCTYPE html>
-<html lang="fr-FR">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo $title ?></title>
-        <link rel="icon" href="././public/image/favicon.ico">
-        <link href="./public/css/style.css" rel="stylesheet"/>
-        <script src="https://kit.fontawesome.com/158c254b57.js"></script>
-        <script async src="./app.js" defer></script>
-    </head>
-        
+?>        
     <!--<body onselectstart="return false" oncontextmenu="return false" 
     ondragstart="return false" onMouseOver="window.status='No right click '; return true;">-->
     <body>
@@ -42,5 +27,4 @@ require('./app/view/contactModalForm.php');
             <?php echo '<div class="modal" id="contactModal">' . $contactModalForm . '</div>'?>
         </div>
     </body>
-</html>
 
--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 
 <?php
-
 require('./vendor/composer/autoload_files.php');
-require('./app/view/template.php');
-
+require ('./app/textContent.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="keywords" content="développeur web, web developer, création de site web, site web, site internet, web design, webdesigner, site design, design site web, creative web, paris">
+        <meta name="description" content="Yann H. vous aide à concevoir et produire différentes solutions digitales pour votre entreprise : création de site web vitrine, site e-commerce, chat/forum d'entreprise, et pleins d'autres solutions pour vous aider à vous développer grâce aux outils informatiques et la puissance du web.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+
+        <title><?php echo $title ?></title>
+        <link rel="icon" href="././public/image/favicon.ico">
+        <link href="./public/css/style.css" rel="stylesheet"/>
+        <script src="https://kit.fontawesome.com/158c254b57.js"></script>
+        <script async src="./app.js" defer></script>
+    </head>
+
+    <?php require('./app/view/template.php'); ?>
+</html>
