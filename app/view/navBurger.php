c584f34e5fd63ebebd5299c678516be030a890c0
Yann Dev <h.yann_pro@yahoo.fr>
2022-02-14 10:40:21 +0100

    role added to menus

--- app/view/navBurger.php
+++ app/view/navBurger.php
@@ -1,17 +1,17 @@
 <?php ob_start(); ?>
 <div id="draggable_burger_container" data-boxtype="drop" data-appendto="true">
     <label for="toggle" draggable="true" id="draggable_burger">☰</label>
-        <input type="checkbox" id="toggle">
+        <input type="checkbox" id="toggle" aria-haspopup="true" aria-controls="burger_links">
      
-        <div class="burger_links">
-            <a href="#services"><?php echo strtoupper('Services'); ?> </a>
-            <a href="#skills"><?php echo strtoupper('Competences'); ?></a>
-            <a href="#portfolio"><?php echo strtoupper('Portfolio'); ?></a>
-            <a href="#prices"><?php echo strtoupper('Tarifs'); ?></a>
-            <a href="#about"><?php echo strtoupper('A propos'); ?></a>
-            <a href="#contact"><?php echo strtoupper('Contact'); ?></a>
+        <div class="burger_links" role="menu">
+            <a href="#services" role="menuitem"><?php echo strtoupper('Services'); ?> </a>
+            <a href="#skills" role="menuitem"><?php echo strtoupper('Competences'); ?></a>
+            <a href="#portfolio" role="menuitem"><?php echo strtoupper('Portfolio'); ?></a>
+            <a href="#prices" role="menuitem"><?php echo strtoupper('Tarifs'); ?></a>
+            <a href="#about" role="menuitem"><?php echo strtoupper('A propos'); ?></a>
+            <a href="#contact" role="menuitem"><?php echo strtoupper('Contact'); ?></a>
         </div>
-        <div id="arrowAnim" class="hidden">
+        <div id="arrowAnim" class="hidden" aria-hidden="true">
             <div class="arrowSliding">
                 <div class="arrow"></div>
             </div>
--- app/view/navbar.php
+++ app/view/navbar.php
@@ -1,12 +1,12 @@
 <?php ob_start(); ?>
 <div class="leftHolder"></div>
-    <ul>
-        <a href="#services"><li><?php echo strtoupper('Services'); ?> </li></a>
-        <a href="#skills"><li><?php echo strtoupper('Competences'); ?></li></a>
-        <a href="#portfolio"><li><?php echo strtoupper('Portfolio'); ?></li></a>
-        <a href="#prices"><li><?php echo strtoupper('Tarifs'); ?></li></a>
-        <a href="#about"><li><?php echo strtoupper('A propos'); ?></li></a>
-        <a href="#contact"><li><?php echo strtoupper('Contact'); ?></li></a>
+    <ul role="menu">
+        <a href="#services" role="menuitem"><li role="none"><?php echo strtoupper('Services'); ?> </li></a>
+        <a href="#skills" role="menuitem"><li role="none"><?php echo strtoupper('Competences'); ?></li></a>
+        <a href="#portfolio" role="menuitem"><li role="none"><?php echo strtoupper('Portfolio'); ?></li></a>
+        <a href="#prices" role="menuitem"><li role="none"><?php echo strtoupper('Tarifs'); ?></li></a>
+        <a href="#about" role="menuitem"><li role="none"><?php echo strtoupper('A propos'); ?></li></a>
+        <a href="#contact" role="menuitem"><li role="none"><?php echo strtoupper('Contact'); ?></li></a>
         </ul>
 <div class="rightHolder"></div>
 
